51561b3eb7354c2dbfedcad66a609285a9062de5
C.van Ek <2556917@student.vu.nl>
2018-02-27 11:13:48 +0100

    Improved class diagram

---
+++ Workspace/Simbad-Project-Skeleton/src/main/java/softdesign/Message.java
@@ -0,0 +1,10 @@
+package main.java.softdesign;
+
+import javax.vecmath.Vector3d;
+
+public class Message {
+	public String data;
+	public String originName;
+	public Vector3d originLocation;
+	public int receivedAt;
+}
